2236b03170e600518640955850dec5939f410648
ksteensig <kasper.steensig@gmail.com>
2020-10-08 17:54:15 +0200

    Add comments from supervisor meeting

--- esprit.py
+++ esprit.py
@@ -1,15 +1,18 @@
 import numpy as np
 import numpy.linalg as la
 import numpy.random as rand
-import scipy.io
+#import scipy.io
 
-def generate_los_ula_data(N, K, f):
+# needs an SNR parameter, to compute the noise variance
+def generate_los_ula_data(N, K, T, f):
     c = 3e8 # speed of light
     wl = c/f # wavelength (lambda)
     d = wl/2 # uniform distance between antennas
     
     theta = rand.uniform(0, np.pi, (K,1))
-    alpha = 3*rand.uniform(size=(K,1))
+    
+    # make this complex normal distributed
+    alpha = rand.uniform(size=(K,1))
     
     H = np.zeros((N,K), dtype = 'complex_')
 
@@ -19,47 +22,61 @@ def generate_los_ula_data(N, K, f):
         for k in range(K):
             angle = theta[k]
             attenuation = alpha[k]
-            H[n,k] = attenuation*np.exp(spatial_const*np.sin(angle))
+            H[n,k] = attenuation*np.exp(spatial_const*np.cos(angle))
             
-    s = rand.randn(K,1)
-    n = rand.randn(N,1)
+    S = 100*np.ones((K,T)) #rand.randn(K,5)
+    
+    # should be complex normal
+    N = rand.randn(N,T)
     
     # y = Hs + n
-    y = H.dot(s) + n
+    Y = H.dot(S) + N
     
-    return theta,s,y
-            
-    
+    return theta,S,Y
 
-def esprit(y, N, K):
-    m = len(y)
+
+def esprit(Y, N, K, f):
+    c = 3e8 # speed of light
+    wl = c/f # wavelength (lambda)
+    d = wl/2 # uniform distance between antennas
     
-    R = np.outer(y, y)
+    m = len(Y)
     
+    # \hat R = E[Y*Y^H] = 1/m * Y*Y^H
+    R = Y.dot(Y.conj().T) / m
+    
+    # ensure matrix is hermitian
+    #R = 0.5 * (R + R.conj().T)
+    
+    # ensure descending order of U, E, V
     U,E,V = la.svd(R)
     
     S = U[:, 0:K]
     
-    S1 = S[0:m-1]
-    S2 = S[1:m]
+    S1 = S[0:m-1,:]
+    S2 = S[1:m,:]
     
+    # P = S1 \ S2
     P = la.pinv(S1) @ S2
     
     # E = eigenvalues, Q = eigenvectors
     eigs,_ = la.eig(P)
     
-    return np.arcsin(np.angle(eigs)/np.pi)
+    return np.arccos(np.angle(eigs)/(2*np.pi*N*d))
 
 
-#x = np.array([np.random.randn(6)]).transpose()
+# antennas
+N = 5
+# users
+K = 2
+# samples
+T = 5
+# frequency
+f = 1e9
 
-#a = esprit(x, 2, 1)
+theta,S,Y = generate_los_ula_data(N, K, T, f)
 
-theta,s,y = generate_los_ula_data(5, 2, 1e9)
-
-scipy.io.savemat('test.mat', dict(y=y, theta=theta, s=s))
-
-theta_hat = esprit(y, 5, 2)
+theta_hat = esprit(Y, N, K, f)
 
 print(theta)
 print(theta_hat)

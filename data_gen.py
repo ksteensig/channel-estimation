a394731804c0ca51082ed63158426983b800d7c1
Kasper Steensig Jensen <kasper.steensig@gmail.com>
2020-10-17 22:26:55 +0200

    Solve issue with data generation, it was all in the normalization of the training data

--- data_gen.py
+++ data_gen.py
@@ -5,19 +5,19 @@ import matplotlib.pyplot as plt
 
 def array_response_vector(array,theta):
     N = array.shape
-    v = np.exp(1j*2*np.pi*array*np.sin(theta))
+    v = np.exp(1j*2*np.pi*array*np.cos(theta))
     return v/np.sqrt(N)
 
 def generate_los_ula_data(N, K, T, SNR, f):
     c = 3e8 # speed of light
     wl = c/f # wavelength (lambda)
     d = wl/2 # uniform distance between antennas
-    theta = rand.uniform(-np.pi/2, np.pi/2, (K))
 
-    snr = 10 #rand.uniform(SNR[0], SNR[1])
+    snr = rand.uniform(SNR[0], SNR[1])
     
     array = np.linspace(0,N-1,N)*d/wl
 
+    theta = np.sort(np.pi*np.random.rand(K))   # random source directions
     alpha = np.random.randn(K) + np.random.randn(K)*1j # random source powers
     alpha = np.sqrt(1/2)*alpha
 
@@ -27,12 +27,10 @@ def generate_los_ula_data(N, K, T, SNR, f):
         htmp = np.zeros(N)
         for i in range(K):
             pha = np.exp(1j*2*np.pi*np.random.rand(1))
-            htmp = htmp + alpha[i]*pha*array_response_vector(array,theta[i])
+            htmp = htmp + pha*alpha[i]*array_response_vector(array,theta[i])
         H[:,iter] = htmp + np.sqrt(0.5/snr)*(np.random.randn(N)+np.random.randn(N)*1j)
 
-    theta = np.repeat(theta, T).reshape(K,T).T
-
-    return np.sort(theta),H
+    return np.repeat(theta, T).reshape(K,T).T,H
 
 def esprit(CovMat,L,N):
     # CovMat is the signal covariance matrix, L is the number of sources, N is the number of antennas
@@ -40,25 +38,26 @@ def esprit(CovMat,L,N):
     S = U[:,0:L]
     Phi = la.pinv(S[0:N-1]) @ S[1:N] # the original array is divided into two subarrays [0,1,...,N-2] and [1,2,...,N-1]
     eigs,_ = la.eig(Phi)
-    DoAsESPRIT = np.arcsin(np.angle(eigs)/np.pi)
-    return np.sort(DoAsESPRIT)
+    DoAsESPRIT = np.arccos(np.angle(eigs)/np.pi)
+    return DoAsESPRIT
 
+# testing: should result in MSE of 7.210449609028078e-06
 """
+np.random.seed(6)
+
 # antennas
-N = 128
+N = 32
 
 # users
-K = 16
+K = 5
 
 # bits
-T = 16
+T = 100
 
 # frequency
 f = 1e9
 
-SNR = [10,10]
-
-#np.random.seed(1)
+SNR = [5,30]
 
 theta,H = generate_los_ula_data(N, K, T, SNR, f)
 
@@ -78,7 +77,5 @@ print(mse)
 
 E,U = la.eig(CovMat)
 
-#plt.plot(np.abs(E))
-#plt.show()
+plt.plot(np.abs(E))
 """
-

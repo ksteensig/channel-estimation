67dee46270a05f525c1305494269f3142f75bfee
Kasper Steensig Jensen <kasper.steensig@gmail.com>
2020-10-20 19:59:28 +0200

    Fix data generation

--- data_gen.py
+++ data_gen.py
@@ -1,81 +1,83 @@
 import numpy as np
 import numpy.random as rand
 import numpy.linalg as la
-import matplotlib.pyplot as plt
 
-def array_response_vector(array,theta):
-    N = array.shape
-    v = np.exp(1j*2*np.pi*array*np.cos(theta))
-    return v/np.sqrt(N)
 
-def generate_los_ula_data(N, K, T, SNR, f):
+# SNR is a range between min and max SNR in dB
+def generate_los_ula_data(N, K, L, SNR, f):
     c = 3e8 # speed of light
     wl = c/f # wavelength (lambda)
     d = wl/2 # uniform distance between antennas
 
-    snr = rand.uniform(SNR[0], SNR[1])
+    # choose random snr
+    db2pow = 10**(rand.uniform(SNR[0], SNR[1])/10)
     
+    # antenna array
     array = np.linspace(0,N-1,N)*d/wl
+    
+    # steering vector
+    array_response = lambda array,theta: np.exp(-1j*2*np.pi*array*np.cos(theta))
 
-    theta = np.sort(np.pi*np.random.rand(K))   # random source directions
-    alpha = np.random.randn(K) + np.random.randn(K)*1j # random source powers
-    alpha = np.sqrt(1/2)*alpha
+    theta = np.sort(np.pi*np.random.rand(K))
+    alpha = (np.random.randn(K,L) + 1j*np.random.randn(K,L))*np.sqrt(1/2)
+    
+    x = np.exp(1j*2*np.pi*np.random.rand(K))
+    
+    Y = np.zeros((N,L)) + 1j*np.zeros((N,L))
+        
+    # assume the source vector x is a vector containing ones
+    # i.e. x = [1 1 ... 1]^T
+    for l in range(L):
+        for k in range(K):
+            response = array_response(array, theta[k])
+            Y[:,l] += alpha[k,l]*response*x[k]#*np.exp(-1j*2*np.pi*np.random.rand(1))
+    # N = [n1 n2 .. nL]
+    N = (rand.randn(N,L) + 1j*rand.randn(N,L))*np.sqrt(0.5/db2pow)
 
-    H = np.zeros((N,T)) + 1j*np.zeros((N,T))
+    # y = Hx + n
+    Y += N
+        
+    return theta, Y
 
-    for iter in range(T):
-        htmp = np.zeros(N)
-        for i in range(K):
-            pha = np.exp(1j*2*np.pi*np.random.rand(1))
-            htmp = htmp + pha*alpha[i]*array_response_vector(array,theta[i])
-        H[:,iter] = htmp + np.sqrt(0.5/snr)*(np.random.randn(N)+np.random.randn(N)*1j)
-
-    return np.repeat(theta, T).reshape(K,T).T,H
-
-def esprit(CovMat,L,N):
-    # CovMat is the signal covariance matrix, L is the number of sources, N is the number of antennas
-    _,U = la.eig(CovMat)
-    S = U[:,0:L]
-    Phi = la.pinv(S[0:N-1]) @ S[1:N] # the original array is divided into two subarrays [0,1,...,N-2] and [1,2,...,N-1]
-    eigs,_ = la.eig(Phi)
-    DoAsESPRIT = np.arccos(np.angle(eigs)/np.pi)
-    return DoAsESPRIT
-
-# testing: should result in MSE of 7.210449609028078e-06
 """
-np.random.seed(6)
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 # antennas
-N = 32
+N = 128
 
 # users
-K = 5
+K = 8
 
 # bits
-T = 100
+L = 100
+
+# SNR min and max
+snr = [20, 20]
 
 # frequency
 f = 1e9
 
-SNR = [5,30]
+theta, Y = generate_los_ula_data(N, K, L, snr, f)
 
-theta,H = generate_los_ula_data(N, K, T, SNR, f)
+C = np.cov(Y)
 
-theta = theta[0]
+E,U = la.eig(C)
 
-print(theta)
+E = np.abs(E)
+E[::-1].sort()
 
-CovMat = H@H.conj().transpose()
+ax = plt.figure().gca()
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+ax.grid(True)
+#plt.yscale('log')
+plt.semilogy(E)
+plt.show()
 
-theta_hat = np.sort(esprit(CovMat, K, N))
-
-print(theta_hat)
-
-mse = ((theta - theta_hat)**2).mean()
-
-print(mse)
-
-E,U = la.eig(CovMat)
-
-plt.plot(np.abs(E))
+ax = plt.figure().gca()
+ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+plt.grid(color='gray', linestyle='-', linewidth=0.5)
+plt.plot(E)
+plt.show()
+print(E[0:10])
 """

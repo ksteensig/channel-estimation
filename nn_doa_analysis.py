3867836370b8380c8509a50d9ce1282701a0fc02
Kasper Steensig <kasper.steensig@gmail.com>
2020-11-19 11:03:05 +0100

    Improve delta graph

--- nn_doa_analysis.py
+++ nn_doa_analysis.py
@@ -19,21 +19,21 @@ print('TensorFlow Version:', tf.__version__)
 testing_size = 1000
 
 def data_initialization(N, K, L, freq, dist, sort):
-    testing_labels, testing_data = dg.generate_bulk_data(testing_size, N, K, L, freq, dist, sort)
+    labels, data = dg.generate_bulk_data(testing_size, N, K, L, freq, dist, sort)
 
-    return testing_labels, testing_data
+    return labels, data
 
-def apply_wgn(Y):
+def apply_wgn(Y, snr):
     shape = Y.shape
-    db2pow = 10**(30/10)
+    db2pow = 10**(np.random.uniform(snr[0], snr[1])/10)
     
     # N = [n1 n2 .. nL]
     N = np.random.randn(*shape)*np.sqrt(0.5/db2pow)
 
     return Y+N
 
-def normalize(labels, data):
-    data = apply_wgn(data)
+def normalize(labels, data, snr):
+    data = apply_wgn(data, snr)
     labels = (labels - dg.min_theta)/(dg.max_theta - dg.min_theta) # normalize labels to [0,1]
     data = data/np.max(data)
     
@@ -50,6 +50,8 @@ freq = 1e9
 
 # bits
 L = 16
+
+snr = [5, 30]
 
 learning_rate = 0.1
 momentum = 0.9
@@ -73,24 +75,17 @@ fig2.set_size_inches(18, 10)
 fig3, ax3 = plt.subplots(2,2,constrained_layout=True)
 fig3.set_size_inches(18, 10)
 
-
-tf.keras.utils.plot_model(
-    model, to_file='model.png', show_shapes=True, show_layer_names=True,
-    rankdir='TB', expand_nested=False, dpi=96
-)
-
-
 for dist in ['uniform','zeros','ones']:
-    testing_labels, testing_data = data_initialization(N, K, L, freq, dist, True)
-    testing_labels, testing_data = normalize(testing_labels, testing_data)
+    labels, data = data_initialization(N, K, L, freq, dist, True)
+    labels, data = normalize(labels, data, snr)
     
-    #loss, mse = model.evaluate(testing_data, testing_labels, verbose=2)
+    #loss, mse = model.evaluate(data, labels, verbose=2)
     
     #print(mse)
     
-    m = model.predict(testing_data)
+    m = model.predict(data)
     k = m*np.pi - pi/2
-    testing_labels = testing_labels*np.pi - pi/2
+    labels = labels*np.pi - pi/2
     
     p = pos[dist]
     r = p[0]
@@ -104,19 +99,25 @@ for dist in ['uniform','zeros','ones']:
     ax2[r,c].set_xlabel('Theta_n')
     ax2[r,c].set_ylabel('Mean')
     
-    for i in range(K):
-        #ax[0,0].set_ylim(-0.2, 0.2)
-        ax[r,c].plot((i+1)*np.ones_like(k[:,i].var()), k[:,i].var(), 'x', color='b')
-        ax[r,c].plot((i+1)*np.ones_like(testing_labels[:,i].var()), testing_labels[:,i].var(), 'o', color='r')
-        ax2[r,c].plot((i+1)*np.ones_like(k[:,i].mean()), k[:,i].mean(), 'x', color='b')
-        ax2[r,c].plot((i+1)*np.ones_like(testing_labels[:,i].mean()), testing_labels[:,i].mean(), 'o', color='r')
-    
     ax3[r,c].set_title(dist)
-    ax3[r,c].set_xlabel('Mean difference of Theta_n and Theta_{n-1}')
-    ax3[r,c].set_ylim(-0.2, 0.2)
-    ax3[r,c].plot(np.diff(k).mean(axis=0), np.zeros_like(np.diff(k).mean(axis=0)) + 0.1, 'x', color='b')
-    ax3[r,c].plot(np.diff(testing_labels).mean(axis=0), np.zeros_like(np.diff(testing_labels).mean(axis=0)) - 0.1, 'o', color='r')
-
+    ax3[r,c].set_xlabel('Theta_n')
+    ax3[r,c].set_ylabel('Mean abs delta theta')
+    ax3[r,c].set_ylim([-0.01, 0.16])
+    
+    # plot mean and variance
+    for i in range(K):
+        ax[r,c].plot(i+1, k[:,i].var(), 'x', color='b')
+        ax[r,c].plot(i+1, labels[:,i].var(), 'o', color='r')
+        ax2[r,c].plot(i+1, k[:,i].mean(), 'x', color='b')
+        ax2[r,c].plot(i+1, labels[:,i].mean(), 'o', color='r')
+    
+    # plot mean absolute difference between consecutive theta bins
+    for i in range(1, K):
+        diff_x = np.abs(k[:,i-1] - k[:,i]).mean()
+        diff_o = np.abs(labels[:,i-1] - labels[:,i]).mean()
+        ax3[r,c].plot(i+1, diff_x, 'x', color='b')
+        ax3[r,c].plot(i+1, diff_o, 'o', color='r')
+    
 fig.show()
 fig2.show()
 fig3.show()

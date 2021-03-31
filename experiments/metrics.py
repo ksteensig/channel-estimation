3f51d3d02682dfafb2794e2ef5115116751014c1
Kasper Steensig Jensen <kasper.steensig@gmail.com>
2021-03-31 14:40:57 +0200

    Add notebooks

--- experiments/metrics.py
+++ experiments/metrics.py
@@ -11,8 +11,11 @@ def compute_pos_acc(y_true, y_pred):
     acc_ = 0
     
     for i in range(len(y_true)):
-        idx = np.where(y_true[i] == 1)[0]
-        r = np.sum(y_pred[i][idx])/np.sum(y_true[i][idx])
+        idx_tp = np.where(y_true[i] == 1)[0]
+        idx_tn = np.where(y_true[i] == 0)[0]
+        r = np.sum(y_pred[i][idx_tp]) + np.sum(np.abs(y_pred[i][idx_tn] - 1))
+        r = r/180
+        
         acc_ = acc_ + r
         
     return acc_/len(y_true)
